85d5acfe1d619b78294f0635d3314d5e9a4633c1
Kaustubh Agrawal <kaustubhagrawal8838@gmail.com>
2022-12-04 22:21:49 +0530

    04: cleanup

--- 04/main.c
+++ 04/main.c
@@ -1,38 +1,64 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include <stdbool.h>
-#include <assert.h>
+#include <stdlib.h>  // atoi
+#include <ctype.h>   // isdigit
+#include <string.h>  // for strncat
+#include <stdbool.h> // true and false
+#include <assert.h>  // assert statement
 
-// Can only consume a maximum of 256 character long int(s).
+// Used to move one step back in a file stream..
+void move_one_step_back(FILE *f)
+{
+  fseek(f, -1L, SEEK_CUR);
+}
+
+// Consumes Integer if given the file input stream
+// Caution: Can only consume a maximum of 256 character long int(s).
 int consume_int(FILE *f)
 {
-  char str[256] = "";
+  char buffer[256] = "";
 
   while (true)
   {
-    const char c = getc(f);
+    char c = getc(f);
 
     if (isdigit(c))
-      strncat(str, &c, 1);
+      // add the character to buffer string..
+      strncat(buffer, &c, 1);
     else
       break;
   }
 
-  fseek(f, -1L, SEEK_CUR);
-  return atoi(str);
+  move_one_step_back(f);
+  return atoi(buffer);
 }
 
-int is_end(FILE *f)
+int is_eof(FILE *f)
 {
   char c = getc(f);
   if (c != EOF)
   {
-    fseek(f, -1L, SEEK_CUR);
+    move_one_step_back(f);
     return false;
   }
   return true;
+}
+
+struct Data
+{
+  int start;
+  int end;
+};
+
+struct Data parse_line(FILE *f)
+{
+  int start = consume_int(f);
+  char _ = getc(f); // consume ","
+  int end = consume_int(f);
+  char __ = getc(f); // consume newline character
+
+  struct Data d = {start, end};
+
+  return d;
 }
 
 int main()
@@ -47,18 +73,16 @@ int main()
 
   while (true)
   {
-    if (is_end(f))
+    if (is_eof(f))
       break;
 
-    int a1 = consume_int(f);
-    char _ = getc(f); // consume ","
-    int a2 = consume_int(f);
-    char __ = getc(f); // consume newline character
+    struct Data first = parse_line(f);
+    struct Data second = parse_line(f);
 
-    int b1 = consume_int(f);
-    char ___ = getc(f); // consume ","
-    int b2 = consume_int(f);
-    char ____ = getc(f); // consume newline character
+    int a1 = first.start;
+    int a2 = first.end;
+    int b1 = second.start;
+    int b2 = second.end;
 
     // part 1 logic...
     if ((a1 <= b1 && a2 >= b2) || (a1 >= b1 && a2 <= b2))
