78cfb0c8878a68da8412976dbf325bb5ed60a7bc
Kaustubh Agrawal <kaustubhagrawal8838@gmail.com>
2022-12-07 09:01:02 +0530

    06: part 2

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -3,18 +3,26 @@ use std::{collections::HashMap, fs};
 #[derive(Debug, Clone)]
 struct SlidingWindow {
     content: Vec<char>,
+    size: usize,
 }
 
 impl SlidingWindow {
-    fn new() -> Self {
-        SlidingWindow { content: vec![] }
+    fn new(size: usize) -> Self {
+        SlidingWindow {
+            content: vec![],
+            size,
+        }
     }
 
     fn handle_push(&mut self, val: char) {
-        if self.content.len() == 4 {
+        if self.content.len() == self.size {
             self.content.remove(0);
         }
         self.content.push(val);
+    }
+
+    fn clear(&mut self) {
+        self.content.clear()
     }
 }
 
@@ -39,20 +47,47 @@ fn main() {
     let input = fs::read_to_string(input_file).expect("Input file reading failed!");
     let sample = fs::read_to_string(sample_file).expect("Sample input file reading failed!");
 
-    let mut sliding_window = SlidingWindow::new();
+    let mut sliding_window_4 = SlidingWindow::new(4);
+    let mut sliding_window_14 = SlidingWindow::new(14);
 
-    for (i, character) in input.char_indices() {
-        sliding_window.handle_push(character);
+    sliding_window_4.clear();
+    sliding_window_14.clear();
 
-        if sliding_window.content.len() == 4 {
-            let cond = get_char_occurances(&sliding_window.content)
-                .iter()
-                .all(|(_char, usize)| *usize == 1);
+    println!("====== Sample input ======");
+    println!(
+        "Part 1: {}, Part 2: {}\n",
+        solve(&mut sliding_window_4, sample.clone()),
+        solve(&mut sliding_window_14, sample.clone())
+    );
 
-            if cond {
-                println!("part 1 .... {}", i + 1);
-                break;
+    sliding_window_4.clear();
+    sliding_window_14.clear();
+
+    println!("====== Input ======");
+    println!(
+        "Part 1: {}, Part 2: {}",
+        solve(&mut sliding_window_4, input.clone()),
+        solve(&mut sliding_window_14, input.clone())
+    );
+
+    sliding_window_4.clear();
+    sliding_window_14.clear();
+
+    fn solve(sliding_window: &mut SlidingWindow, s: String) -> usize {
+        for (i, character) in s.char_indices() {
+            sliding_window.handle_push(character);
+
+            if sliding_window.content.len() == sliding_window.size {
+                let cond = get_char_occurances(&sliding_window.content)
+                    .iter()
+                    .all(|(_char, usize)| *usize == 1);
+
+                if cond {
+                    return i + 1;
+                }
             }
         }
+
+        0
     }
 }
